ea3d561e6d9739ade5ee1e79ff6c355d557b3c50
jktham <jonastham13@gmail.com>
2026-04-28 18:39:04 +0200

    fix chunk data alignment

--- src/world.zig
+++ src/world.zig
@@ -28,7 +28,7 @@ pub const N_CHUNKS = 9; // number of chunks in each direction (x and z)
 pub const N_SUBCHUNKS = 16; // number of subchunks per column (y direction)
 pub const N_BLOCKS = 16; // number of blocks in each direction within a subchunk
 
-// chunk xzy, local yxz
+// chunk xzy, local yzx
 pub var chunks: [N_CHUNKS][N_CHUNKS][N_SUBCHUNKS][N_BLOCKS][N_BLOCKS][N_BLOCKS]u8 = undefined;
 
 pub fn generate() !void {
@@ -66,15 +66,15 @@ pub fn setBlock(x: i32, y: i32, z: i32, block: Block) !void {
     const chunk_z = @divFloor(z, 16);
 
     if (chunk_x < 0 or chunk_x >= N_CHUNKS or chunk_y < 0 or chunk_y >= N_SUBCHUNKS or chunk_z < 0 or chunk_z >= N_CHUNKS) {
-        std.log.warn("Attempted to set block outside of world bounds at ({}, {}, {})", .{ x, y, z });
+        std.log.err("Attempted to set block outside of world bounds at ({}, {}, {})", .{ x, y, z });
         return error.OutOfBounds;
     }
 
-    const local_x = @mod(x, 16);
+    const local_x = @mod(7 - x, 16); // alignment with chunk data
     const local_y = @mod(y, 16);
     const local_z = @mod(z, 16);
 
-    chunks[@intCast(chunk_x)][@intCast(chunk_z)][@intCast(chunk_y)][@intCast(local_y)][@intCast(local_x)][@intCast(local_z)] = @intFromEnum(block);
+    chunks[@intCast(chunk_x)][@intCast(chunk_z)][@intCast(chunk_y)][@intCast(local_y)][@intCast(local_z)][@intCast(local_x)] = @intFromEnum(block);
 }
 
 pub fn getBlock(x: i32, y: i32, z: i32) !Block {
@@ -83,21 +83,21 @@ pub fn getBlock(x: i32, y: i32, z: i32) !Block {
     const chunk_z = @divFloor(z, 16);
 
     if (chunk_x < 0 or chunk_x >= N_CHUNKS or chunk_y < 0 or chunk_y >= N_SUBCHUNKS or chunk_z < 0 or chunk_z >= N_CHUNKS) {
-        std.log.warn("Attempted to get block outside of world bounds at ({}, {}, {})", .{ x, y, z });
+        std.log.err("Attempted to get block outside of world bounds at ({}, {}, {})", .{ x, y, z });
         return error.OutOfBounds;
     }
 
-    const local_x = @mod(x, 16);
+    const local_x = @mod(7 - x, 16); // alignment with chunk data
     const local_y = @mod(y, 16);
     const local_z = @mod(z, 16);
 
-    return @enumFromInt(chunks[@intCast(chunk_x)][@intCast(chunk_z)][@intCast(chunk_y)][@intCast(local_y)][@intCast(local_x)][@intCast(local_z)]);
+    return @enumFromInt(chunks[@intCast(chunk_x)][@intCast(chunk_z)][@intCast(chunk_y)][@intCast(local_y)][@intCast(local_z)][@intCast(local_x)]);
 }
 
 // pointer to flat array of 4096 blocks in the subchunk, for direct writing to network buffer in local yxz order
 pub fn getChunkPointer(chunk_x: i32, chunk_y: i32, chunk_z: i32) !*[4096]u8 {
     if (chunk_x < 0 or chunk_x >= N_CHUNKS or chunk_y < 0 or chunk_y >= N_SUBCHUNKS or chunk_z < 0 or chunk_z >= N_CHUNKS) {
-        std.log.warn("Attempted to get chunk pointer outside of world bounds at ({}, {}, {})", .{ chunk_x, chunk_y, chunk_z });
+        std.log.err("Attempted to get chunk pointer outside of world bounds at ({}, {}, {})", .{ chunk_x, chunk_y, chunk_z });
         return error.OutOfBounds;
     }
     return @ptrCast(&chunks[@intCast(chunk_x)][@intCast(chunk_z)][@intCast(chunk_y)]);
